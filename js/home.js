6b1cd20e6ed42c48536774456ce414e4f8991529
nicholas gregory <nick.gregory@jet.com>
2016-09-10 14:41:31 -0400

    init

---
+++ js/home.js
@@ -0,0 +1,539 @@
+/*
+ * jQuery.appear
+ * https://github.com/bas2k/jquery.appear/
+ * http://code.google.com/p/jquery-appear/
+ *
+ * Copyright (c) 2009 Michael Hixson
+ * Copyright (c) 2012 Alexander Brovikov
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+ */
+(function($) {
+    $.fn.appear = function(fn, options) {
+
+        var settings = $.extend({
+
+            //arbitrary data to pass to fn
+            data: undefined,
+
+            //call fn only on the first appear?
+            one: true,
+
+            // X & Y accuracy
+            accX: 0,
+            accY: 0
+
+        }, options);
+
+        return this.each(function() {
+
+            var t = $(this);
+
+            //whether the element is currently visible
+            t.appeared = false;
+
+            if (!fn) {
+
+                //trigger the custom event
+                t.trigger('appear', settings.data);
+                return;
+            }
+
+            var w = $(window);
+
+            //fires the appear event when appropriate
+            var check = function() {
+
+                //is the element hidden?
+                if (!t.is(':visible')) {
+
+                    //it became hidden
+                    t.appeared = false;
+                    return;
+                }
+
+                //is the element inside the visible window?
+                var a = w.scrollLeft();
+                var b = w.scrollTop();
+                var o = t.offset();
+                var x = o.left;
+                var y = o.top;
+
+                var ax = settings.accX;
+                var ay = settings.accY;
+                var th = t.height();
+                var wh = w.height();
+                var tw = t.width();
+                var ww = w.width();
+
+                if (y + th + ay >= b &&
+                    y <= b + wh + ay &&
+                    x + tw + ax >= a &&
+                    x <= a + ww + ax) {
+
+                    //trigger the custom event
+                    if (!t.appeared) t.trigger('appear', settings.data);
+
+                } else {
+
+                    //it scrolled out of view
+                    t.appeared = false;
+                }
+            };
+
+            //create a modified fn with some additional logic
+            var modifiedFn = function() {
+
+                //mark the element as visible
+                t.appeared = true;
+
+                //is this supposed to happen only once?
+                if (settings.one) {
+
+                    //remove the check
+                    w.unbind('scroll', check);
+                    var i = $.inArray(check, $.fn.appear.checks);
+                    if (i >= 0) $.fn.appear.checks.splice(i, 1);
+                }
+
+                //trigger the original fn
+                fn.apply(this, arguments);
+            };
+
+            //bind the modified fn to the element
+            if (settings.one) t.one('appear', settings.data, modifiedFn);
+            else t.bind('appear', settings.data, modifiedFn);
+
+            //check whenever the window scrolls
+            w.scroll(check);
+
+            //check whenever the dom changes
+            $.fn.appear.checks.push(check);
+
+            //check now
+            (check)();
+        });
+    };
+
+    //keep a queue of appearance checks
+    $.extend($.fn.appear, {
+
+        checks: [],
+        timeout: null,
+
+        //process the queue
+        checkAll: function() {
+            var length = $.fn.appear.checks.length;
+            if (length > 0) while (length--) ($.fn.appear.checks[length])();
+        },
+
+        //check the queue asynchronously
+        run: function() {
+            if ($.fn.appear.timeout) clearTimeout($.fn.appear.timeout);
+            $.fn.appear.timeout = setTimeout($.fn.appear.checkAll, 20);
+        }
+    });
+
+    //run checks when these methods are called
+    $.each(['append', 'prepend', 'after', 'before', 'attr',
+        'removeAttr', 'addClass', 'removeClass', 'toggleClass',
+        'remove', 'css', 'show', 'hide'], function(i, n) {
+        var old = $.fn[n];
+        if (old) {
+            $.fn[n] = function() {
+                var r = old.apply(this, arguments);
+                $.fn.appear.run();
+                return r;
+            }
+        }
+    });
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function($) {
+    $.fn.countTo = function(options) {
+        // merge the default plugin settings with the custom options
+        options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(options.speed / options.refreshInterval),
+            increment = (options.to - options.from) / loops;
+
+        return $(this).each(function() {
+            var _this = this,
+                loopCount = 0,
+                value = options.from,
+                interval = setInterval(updateTimer, options.refreshInterval);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+                $(_this).html(value.toFixed(options.decimals));
+
+                if (typeof(options.onUpdate) == 'function') {
+                    options.onUpdate.call(_this, value);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    value = options.to;
+
+                    if (typeof(options.onComplete) == 'function') {
+                        options.onComplete.call(_this, value);
+                    }
+                }
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,  // the number the element should start at
+        to: 100,  // the number the element should end at
+        speed: 1000,  // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,  // the number of decimal places to show
+        onUpdate: null,  // callback method for every time the element is updated,
+        onComplete: null,  // callback method for when the element finishes updating
+    };
+})(jQuery);
+(function($){
+
+    /* ---------------------------------------------- /*
+     * Preloader
+     /* ---------------------------------------------- */
+
+    $(window).load(function() {
+        $('.loader').fadeOut();
+        $('.page-loader').delay(350).fadeOut('slow');
+    });
+
+    $(document).ready(function() {
+
+        /* ---------------------------------------------- /*
+         * Initialization General Scripts for all pages
+         /* ---------------------------------------------- */
+
+        var homeSection = $('.home-section'),
+            navbar      = $('.navbar-custom'),
+            navHeight   = navbar.height(),
+            worksgrid   = $('#works-grid'),
+            width       = Math.max($(window).width(), window.innerWidth),
+            mobie;
+
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            mobie = true;
+        }
+
+        buildHomeSection(homeSection);
+        navbarAnimation(navbar, homeSection, navHeight);
+        navbarSubmenu(width);
+        hoverDropdown(width, mobie);
+
+        $(window).resize(function() {
+            var width = Math.max($(window).width(), window.innerWidth);
+            buildHomeSection(homeSection);
+            hoverDropdown(width, mobie);
+        });
+
+        $(window).scroll(function() {
+            effectsHomeSection(homeSection, this);
+            navbarAnimation(navbar, homeSection, navHeight);
+        });
+
+        /* ---------------------------------------------- /*
+         * Home section height
+         /* ---------------------------------------------- */
+
+        function buildHomeSection(homeSection) {
+            if (homeSection.length > 0) {
+                if (homeSection.hasClass('home-full-height')) {
+                    homeSection.height($(window).height());
+                } else {
+                    homeSection.height($(window).height() * 0.85);
+                }
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Home section effects
+         /* ---------------------------------------------- */
+
+        function effectsHomeSection(homeSection, scrollTopp) {
+            if (homeSection.length > 0) {
+                var homeSHeight = homeSection.height();
+                var topScroll = $(document).scrollTop();
+                if ((homeSection.hasClass('home-parallax')) && ($(scrollTopp).scrollTop() <= homeSHeight)) {
+                    homeSection.css('top', (topScroll * 0.55));
+                }
+                if (homeSection.hasClass('home-fade') && ($(scrollTopp).scrollTop() <= homeSHeight)) {
+                    var caption = $('.caption-content');
+                    caption.css('opacity', (1 - topScroll/homeSection.height() * 1));
+                }
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Intro slider setup
+         /* ---------------------------------------------- */
+
+        if( $('.hero-slider').length > 0 ) {
+            $('.hero-slider').flexslider( {
+                animation: "fade",
+                animationSpeed: 1000,
+                animationLoop: true,
+                prevText: '',
+                nextText: '',
+                before: function(slider) {
+                    $('.hs-caption').fadeOut().animate({top:'-80px'},{queue:false, easing: 'swing', duration: 700});
+                    slider.slides.eq(slider.currentSlide).delay(500);
+                    slider.slides.eq(slider.animatingTo).delay(500);
+                },
+                after: function(slider) {
+                    $('.hs-caption').fadeIn().animate({top:'0'},{queue:false, easing: 'swing', duration: 700});
+                },
+                useCSS: true
+            });
+        }
+
+
+
+        /* ---------------------------------------------- /*
+         * Transparent navbar animation
+         /* ---------------------------------------------- */
+
+        function navbarAnimation(navbar, homeSection, navHeight) {
+            var topScroll = $(window).scrollTop();
+            if (navbar.length > 0 && homeSection.length > 0) {
+                if(topScroll >= navHeight) {
+                    navbar.removeClass('navbar-transparent');
+                } else {
+                    navbar.addClass('navbar-transparent');
+                }
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Navbar submenu
+         /* ---------------------------------------------- */
+
+        function navbarSubmenu(width) {
+            if (width > 767) {
+                $('.navbar-custom .navbar-nav > li.dropdown').hover(function() {
+                    var MenuLeftOffset  = $('.dropdown-menu', $(this)).offset().left;
+                    var Menu1LevelWidth = $('.dropdown-menu', $(this)).width();
+                    if (width - MenuLeftOffset < Menu1LevelWidth * 2) {
+                        $(this).children('.dropdown-menu').addClass('leftauto');
+                    } else {
+                        $(this).children('.dropdown-menu').removeClass('leftauto');
+                    }
+                    if ($('.dropdown', $(this)).length > 0) {
+                        var Menu2LevelWidth = $('.dropdown-menu', $(this)).width();
+                        if (width - MenuLeftOffset - Menu1LevelWidth < Menu2LevelWidth) {
+                            $(this).children('.dropdown-menu').addClass('left-side');
+                        } else {
+                            $(this).children('.dropdown-menu').removeClass('left-side');
+                        }
+                    }
+                });
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Navbar hover dropdown on desktop
+         /* ---------------------------------------------- */
+
+        function hoverDropdown(width, mobie) {
+            if ((width > 767) && (mobie !== true)) {
+                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').removeClass('open');
+                var delay = 0;
+                var setTimeoutConst;
+                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').hover(function() {
+                        var $this = $(this);
+                        setTimeoutConst = setTimeout(function() {
+                            $this.addClass('open');
+                            $this.find('.dropdown-toggle').addClass('disabled');
+                        }, delay);
+                    },
+                    function() {
+                        clearTimeout(setTimeoutConst);
+                        $(this).removeClass('open');
+                        $(this).find('.dropdown-toggle').removeClass('disabled');
+                    });
+            } else {
+                $('.navbar-custom .navbar-nav > li.dropdown, .navbar-custom li.dropdown > ul > li.dropdown').unbind('mouseenter mouseleave');
+                $('.navbar-custom [data-toggle=dropdown]').not('.binded').addClass('binded').on('click', function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    $(this).parent().siblings().removeClass('open');
+                    $(this).parent().siblings().find('[data-toggle=dropdown]').parent().removeClass('open');
+                    $(this).parent().toggleClass('open');
+                });
+            }
+        }
+
+        /* ---------------------------------------------- /*
+         * Navbar collapse on click
+         /* ---------------------------------------------- */
+
+        $(document).on('click','.navbar-collapse.in',function(e) {
+            if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+                $(this).collapse('hide');
+            }
+        });
+
+        /* ---------------------------------------------- /*
+         * Set sections backgrounds
+         /* ---------------------------------------------- */
+
+        var module = $('.home-section, .module, .module-small, .side-image');
+        module.each(function(i) {
+            if ($(this).attr('data-background')) {
+                $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
+            }
+        });
+
+        /* ---------------------------------------------- /*
+         * Progress bar animations
+         /* ---------------------------------------------- */
+
+        $('.progress-bar').each(function(i) {
+            $(this).appear(function() {
+                var percent = $(this).attr('aria-valuenow');
+                $(this).animate({'width' : percent + '%'});
+                $(this).find('span').animate({'opacity' : 1}, 900);
+                $(this).find('span').countTo({from: 0, to: percent, speed: 900, refreshInterval: 30});
+            });
+        });
+
+        $('.count-item').each(function(i) {
+            $(this).appear(function() {
+                var number = $(this).find('.count-to').data('countto');
+                $(this).find('.count-to').countTo({from: 0, to: number, speed: 1200, refreshInterval: 30});
+            });
+        });
+
+
+
+        /* ---------------------------------------------- /*
+         * Owl slider
+         /* ---------------------------------------------- */
+
+        $('.owl-carousel').each(function(i) {
+
+            // Check items number
+            if ($(this).data('items') > 0) {
+                items = $(this).data('items');
+            } else {
+                items = 4;
+            }
+
+            // Check pagination true/false
+            if (($(this).data('pagination') > 0) && ($(this).data('pagination') === true)) {
+                pagination = true;
+            } else {
+                pagination = false;
+            }
+
+            // Check navigation true/false
+            if (($(this).data('navigation') > 0) && ($(this).data('navigation') === true)) {
+                navigation = true;
+            } else {
+                navigation = false;
+            }
+
+            // Build carousel
+            $(this).owlCarousel( {
+                navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+                navigation: navigation,
+                pagination: pagination,
+                paginationSpeed: 400,
+                singleItem: false,
+                items: items,
+                slideSpeed: 300,
+                autoPlay: 5000
+            });
+
+        });
+
+
+        /* ---------------------------------------------- /*
+         * WOW Animation When You Scroll
+         /* ---------------------------------------------- */
+
+        wow = new WOW({
+            mobile: false
+        });
+        wow.init();
+
+        /* ---------------------------------------------- /*
+         * Rotate
+         /* ---------------------------------------------- */
+
+        $(".rotate").textrotator({
+            animation: "dissolve",
+            separator: "|",
+            speed: 3000
+        });
+
+
+
+        /* ---------------------------------------------- /*
+         * Open tabs by external link
+         /* ---------------------------------------------- */
+
+        $('.open-tab').click(function (e) {
+            var pattern = /#.+/gi;
+            var contentID = e.target.toString().match(pattern)[0];
+            $('.nav-tabs a[href="' + contentID + '"]').tab('show');
+        });
+
+        /* ---------------------------------------------- /*
+         * Scroll Animation
+         /* ---------------------------------------------- */
+
+        $('.section-scroll').bind('click', function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+        /* ---------------------------------------------- /*
+         * Scroll top
+         /* ---------------------------------------------- */
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+
+        $('a[href="#totop"]').click(function() {
+            $('html, body').animate({ scrollTop: 0 }, 'slow');
+            return false;
+        });
+
+
+    });
+
+})(jQuery);
